4cd967710ea9ca975a6dfdc57fcf55f9333f3296
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-01-11 21:20:22 -0300

    add screen countries and component Header--- add packages {"css-loader "html-webpack "live-server "style-loader "webpack": "webpack-cli "webpack-dev-server}

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+ const Header = {
+    render: () => {
+        return `
+        <div>
+            <p>Where in the world?</p>
+        </div>
+        <div>
+            <p>Mode</p>
+        </div>
+        `
+    },
+    after_render: () => {},
+}
+
+export default Header;
---
+++ frontend/src/screens/CountriesScreen.js
@@ -0,0 +1,6 @@
+const CountriesScreen = {
+    render: () =>{
+        return`<h1>prueba</h1>`
+    }
+} 
+export default CountriesScreen;
