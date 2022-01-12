b381af3af9b5ac37f0b4cee0783b04c426f99b18
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-01-11 21:21:14 -0300

    files

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,32 +1,32 @@
-export const loadCountries = async() => {
+const loadCountries = async() => {
     try{
-        const respuesta =  await fetch(`https://restcountries.com/v3.1/all`)
+        const respuesta =  await fetch(`https://restcountries.com/v2/all?fields=name,capital,region,population`)
         console.log(respuesta);
         //si es correcta 
         if (respuesta.status === 200){
             const datos = await respuesta.json();
             console.log(datos.sort());
 
-            let card_countries = '';
+            // let card_countries = '';
 
-            datos.forEach(element => {
-                console.log(element)
-                card_countries += `
-                <section>
-                <aside>
-                    <img src="" alt="">
-                </aside>
-                <div>
-                    <a href="http:/index/${element.translations.spa.common}"> <h4>${element.translations.spa.common}</h4></a>
-                    <p>${element.population}</p>
-                    <p>${element.region}</p>
-                    <p>${element.capital}</p>
-                    <p></p>
-                </div>
-                </section>
-                ` ;
-                document.getElementById('contenedor').innerHTML=card_countries;
-            });
+            // datos.forEach(element => {
+            //     console.log(element)
+            //     card_countries += `
+            //     <section>
+            //     <aside>
+            //         <img src="" alt="">
+            //     </aside>
+            //     <div>
+            //         <a href="http:/index/${element.translations.spa.common}"> <h4>${element.translations.spa.common}</h4></a>
+            //         <p>${element.population}</p>
+            //         <p>${element.region}</p>
+            //         <p>${element.capital}</p>
+            //         <p></p>
+            //     </div>
+            //     </section>
+            //     ` ;
+            //     document.getElementById('contenedor').innerHTML=card_countries;
+            // });
         } 
         else if (respuesta.status === 401) {
             console.log('pusite algo mal')
@@ -44,3 +44,4 @@ export const loadCountries = async() => {
     
 };
 
+loadCountries()
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,30 @@
+import Header from './components/Header.js';
 
+import { parseRequestUrl } from "./utils.js";
+import './style.css';
+import  CountriesScreen  from './screens/CountriesScreen.js';
 
-console.log(window.location.pathname)
+const routes = {
+    '/':CountriesScreen,
+};
+const router = async() => {
+    const request = parseRequestUrl();
+    console.log(request)
+    const parseUrl = (request.resource ? `/${request.resource}` : '/') + (request.id ? `/:id` : ``) + (request.verb ? `/${request.verb}` : ``);
+    console.log(parseUrl)
+    const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+
+    const header = document.getElementById('header_container');
+    header.innerHTML = await Header.render();
+    if (Header.after_render) await Header.after_render();
+
+    const  main = document.getElementById('main_container');
+    main.innerHTML = await screen.render();
+    if (screen.after_render) await screen.after_render();
+}
+
+// console.log(request)
+// console.log(window.location.pathname)
+window.addEventListener("load",router)
+
+window.addEventListener('hashchange',router)
