70573549251c37764a375df2ecd9b56b8b294e7b
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-01-11 22:17:02 -0300

    add dark mode

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,21 @@
- const Header = {
+const Header = {
     render: () => {
         return `
         <div>
             <p>Where in the world?</p>
         </div>
         <div>
-            <p>Mode</p>
+            <button id="boton">Mode</button>
         </div>
         `
     },
-    after_render: () => {},
+    after_render: () => {
+        const boton = document.querySelector('#boton');
+        boton.addEventListener('click', () => {
+            console.log('diste click')
+            document.body.classList.toggle('dark-theme')
+        });
+    },
 }
 
 export default Header;
