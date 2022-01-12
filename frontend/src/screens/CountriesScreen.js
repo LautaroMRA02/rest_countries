a542af45d76897d9b14c9d9f056fc855a83861dc
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-01-11 22:17:11 -0300

    >>

--- frontend/src/screens/CountriesScreen.js
+++ frontend/src/screens/CountriesScreen.js
@@ -1,6 +1,20 @@
 const CountriesScreen = {
     render: () =>{
-        return`<h1>prueba</h1>`
+        return`
+        <form class="filter_countries" name="formulario">
+             <input type="text" list="items" /> 
+             <!-- Lista de opciones --> 
+             <datalist id="items">
+                    <option value="1">Opción 1</option>
+                    <option value="2">Opción 2</option> 
+                    <option value="3">Opción 3</option> </datalist> 
+            <select name="combo"> 
+                <option value="" disabled selected>filter by Region</option> 
+                <option value="1">Opción 1</option> 
+            </select>
+        </form>
+        
+        <h1>prueba</h1>`
     }
 } 
 export default CountriesScreen;
